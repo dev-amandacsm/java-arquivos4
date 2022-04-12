373b7c7b8dfbc1c379ea7b5caede846079d12093
Amanda Muniz <dev.amandacsm@gmail.com>
2022-04-12 10:08:09 -0300

    Uploaded Project

---
+++ src/application/Program.java
@@ -0,0 +1,24 @@
+package application;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Program {
+    public static void main(String[] args) {
+        String[] lines = new String[] {"Good morning", "Good afternoon", "Good night"};
+
+        String path = "C:\\workspaces\\out.txt";
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
+            for(String line : lines){
+                bw.write(line);
+                bw.newLine(); //adiciona quebra de linha
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
